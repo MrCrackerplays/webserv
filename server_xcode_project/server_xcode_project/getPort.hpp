42787739aa8fe288ecacfaffea7d3729d301ed05
Julia Demura <yuliia@Julias-MacBook-Air.local>
2023-01-12 13:03:37 +0100

    update, start work, create first class to store atributes and metods for getaddress function

--- server_xcode_project/server_xcode_project/getPort.hpp
+++ server_xcode_project/server_xcode_project/getPort.hpp
@@ -9,5 +9,32 @@
 #define getPort_hpp
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+class Port{
+	
+private:
+//	std::string _port;
+	struct addrinfo *_res; //is linked list
+	struct addrinfo _hints;
+	
+	
+public:
+	Port();
+	~Port();
+//	void	getPort();
+//	void	getAddrss();
+	void	getRes();
+	void	getHints();
+	
+	
+	
+	
+};
 
 #endif /* getPort_hpp */
