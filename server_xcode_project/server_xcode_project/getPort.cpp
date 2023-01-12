92e62864715e7e20dc8ad14898ffca6722e4ce6e
Julia Demura <yuliia@Julias-MacBook-Air.local>
2023-01-12 13:02:21 +0100

    update xcode, start of work

---
+++ server_xcode_project/server_xcode_project/getPort.cpp
@@ -0,0 +1,8 @@
+//
+//  getPort.cpp
+//  server_xcode_project
+//
+//  Created by Julia Demura on 11/01/2023.
+//
+
+#include "getPort.hpp"
---
+++ server_xcode_project/server_xcode_project/getPort.hpp
@@ -0,0 +1,13 @@
+//
+//  getPort.hpp
+//  server_xcode_project
+//
+//  Created by Julia Demura on 11/01/2023.
+//
+
+#ifndef getPort_hpp
+#define getPort_hpp
+
+#include <stdio.h>
+
+#endif /* getPort_hpp */
