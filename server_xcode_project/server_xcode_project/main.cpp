f83ee8d38bca169edd985ad93fd750efc63d01cd
Yuliia <109362182+ydemura@users.noreply.github.com>
2023-01-12 12:56:41 +0100

    Xcode project (#3)
    * xcode project that will not go into final version to intra repo but that makes my life easier during work.
    setup.
    
    * xcode
    
    Co-authored-by: Julia Demura <yuliia@Julias-MacBook-Air.local>

---
+++ server_xcode_project/server_xcode_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  server_xcode_project
+//
+//  Created by Julia Demura on 09/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
