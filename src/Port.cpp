4a63b3b932d53927f4a080d38f94c24d6c1e3cbd
Yuliia <109362182+ydemura@users.noreply.github.com>
2023-01-13 20:24:15 +0100

    polished Port class wich is rather socket info then port but I leave name Port for now.

--- src/Port.cpp
+++ src/Port.cpp
@@ -1,15 +1,16 @@
 //
-//  getPort.cpp
+//  Port.cpp
 //  server_xcode_project
 //
 //  Created by Julia Demura on 11/01/2023.
 //
 
-#include "getPort.hpp"
+#include "Port.hpp"
 //getaddrinfo, freeaddrinfo, gai_strerror - network address and service translation
 //https://cppsecrets.com/users/18989711511997116104103495564103109971051084699111109/C00-Socket-Programming-getaddrinfo.php
 //https://man7.org/linux/man-pages/man3/getaddrinfo.3.html
 
+//The getaddrinfo() function is used to translate hostnames and service names into a socket address structure, which can be used to create a socket for the server.
 //The addrinfo structure used by getaddrinfo() contains the
 //	   following fields:
 //
@@ -24,13 +25,9 @@
 //			   struct addrinfo *ai_next;
 //		   };
 
-
-
 //	   const char *gai_strerror(int errcode);
 
-
-///This code is a simple example of how to use the getaddrinfo() function in C++ to set up the address and port for a server. The getaddrinfo() function is used to translate hostnames and service names into a socket address structure, which can be used to create a socket for the server.
-///The code does the following:
+//explanation of code:
 ///Declares two variables hints of type struct addrinfo and res which is a pointer to struct addrinfo.
 ///Initialize the hints structure by setting all its bytes to zero using memset() function.
 ///Specify the configuration of the socket:
@@ -42,6 +39,7 @@
 ///If getaddrinfo() succeeded, it returns a pointer to a linked list of struct addrinfo in the res variable.
 ///Once the code is done using the res variable, it frees the memory used by the linked list using the freeaddrinfo() function.
 ///The code then returns 0 to indicate that it has completed successfully.
+
 Port::Port(){
 	
 	int status;
@@ -54,20 +52,22 @@ Port::Port(){
 	
 	
 	status = getaddrinfo(NULL, port_number, &_hints, &_res);
-	
-	
-	
-	
+	if (status < 0){
+		std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+		std::exit(status);
+	}
 }
 
 Port::~Port(){
-	//void freeaddrinfo(struct addrinfo *res);
-	
+	freeaddrinfo(_res);
 }
 
-//void	Port::getPort(){
-//
-//}
+addrinfo Port::getHints(){
+	return _hints;
+}
 
+addrinfo *	Port::getRes(){
+	return _res;
+}
 
 
