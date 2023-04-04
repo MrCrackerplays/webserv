a14b96cac14494af2ff1f3f8e43c3cb9bed05562
Patrick Druart <30552654+MrCrackerplays@users.noreply.github.com>
2023-04-04 14:09:22 +0200

    removed obsolete code

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,32 @@
 #include <fstream>
 #include <poll.h>
 
-
-void	setupSocket(Socket &socket){
-	
+void	setupSocket(Socket &socket) {
 	setToNonBlocking(socket.getSocketFd());
 	bindToPort(socket.getSocketFd(), socket.getAddrInfo());
 	setToListen(socket.getSocketFd());
 	initiateVectPoll(socket.getSocketFd(), socket.getPollFdVector());
 }
 
-void	pollLoop(std::vector<Socket> &vectSockets, std::map<std::string, std::vector<Server> > &servers){
-	
+void	pollLoop(std::vector<Socket> &vectSockets, std::map<std::string, std::vector<Server> > &servers) {
 	for (std::vector<Socket>::iterator it = vectSockets.begin(); it != vectSockets.end(); it++) {
 		try {
 			setupSocket(*it);
 		} catch (std::exception &e) {
-				std::cerr << e.what() << std::endl;
+			std::cerr << e.what() << std::endl;
 		}
 	}
 
 	while (true) {
 		for (std::vector<Socket>::iterator it = vectSockets.begin(); it != vectSockets.end(); it++) {
 			Socket currentSocket = *it;
-			if (poll(&currentSocket.getPollFdVector()[0], (unsigned int)currentSocket.getPollFdVector().size(), 0) < 0){
+			if (poll(&currentSocket.getPollFdVector()[0], (unsigned int)currentSocket.getPollFdVector().size(), 0) < 0) {
 				throw std::runtime_error("Socket : poll");
 			} else {
 				currentSocket.checkEvents();
-				
 			}
 		}
 	}
-	
 }
 
 
@@ -48,31 +43,19 @@ int	main(int argc, char **argv) {
 	}
 	if (argc == 2)
 		config_file = argv[1];
-	
+
 	//test yuliia // comment if not needed :
 	config_file = "/Users/yuliia/Codam/webserv/configs/postuploadtest.conf";
-	
+
 	std::map<std::string, std::vector<Server> > servers;
-	std::map<std::string, Socket> sockets;
 	std::vector<Socket> vectSockets;
 	try {
 		initialize_servers(servers, config_file);
 		std::map<std::string, std::vector<Server> >::iterator it;
-		for (it = servers.begin(); it != servers.end(); it++)
-		{
-			//std::cout << it->second[0].getHost() << ":" << it->second[0].getPort() << std::endl;
-//			sockets.insert(std::pair<std::string, Socket>(it->first,
-			vectSockets.push_back(Socket((char*)it->second[0].getHost().c_str(), (char*)it->second[0].getPort().c_str())) ;
-
+		for (it = servers.begin(); it != servers.end(); it++) {
+			vectSockets.push_back(Socket((char*)it->second[0].getHost().c_str(), (char*)it->second[0].getPort().c_str()));
 		}
 		pollLoop(vectSockets, servers);
-		
-		
-		std::map<std::string, Socket>::iterator it2;
-		for (it2 = sockets.begin(); it2 != sockets.end(); it2++) {
-			std::cout << "Starting socket [" << it2->first << "]" << std::endl;
-			//it2->second.pollLoop(servers);
-		}
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return (1);
